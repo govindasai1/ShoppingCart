33e8524d3ffe54c4f7aa7028c7a8940903849a34
govindasai1 <141134466+govindasai1@users.noreply.github.com>
2023-08-02 12:39:40 +0530

    Add files via upload

---
+++ src/main/kotlin/com/example/ShopingCart/Demo.kt
@@ -0,0 +1,9 @@
+data class Items(var name:String,var price:Int)
+var list1= mutableListOf<Items>(Items("laptop", 50000), Items("phone", 25000))
+fun main(){
+    var amnt=0
+   var  lis2=list1.let { list1.map {amnt+=it.price  } }
+    println(amnt)
+   var result= amnt.let { amnt-((25f/100f)*amnt) }
+    println("..>  $result")
+}
